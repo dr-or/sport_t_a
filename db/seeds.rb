2f029b19fee758c1f3532b486a5c9d561f273193
dr-or <drobinin575@gmail.com>
2023-09-17 19:59:46 +0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+TEAMS = ['Manchester United', 'Real Madrid']
+PLAYER_COUNT = 3
+MATCH_COUNT = 3
+
+PLAYERS = [
+  { first_name: 'Robert', last_name: 'Lewandowski' },
+  { first_name: 'Lionel', last_name: 'Messi' },
+  { first_name: 'Mohamed', last_name: 'Salah' },
+  { first_name: 'Karim', last_name: 'Benzema' },
+  { first_name: 'Kylian', last_name: 'Mbappé' },
+  { first_name: 'Erling', last_name: 'Haaland' }
+]
+
+ACHIEVEMENTS = [
+  'сделал 70+ % точных передач',
+  'пробежал 10+ км'
+]
+
+TEAMS.each_with_index do |team, index|
+  new_team = Team.create(name: team)
+
+  first = index * PLAYER_COUNT
+  last = first + PLAYER_COUNT - 1
+
+  PLAYERS[first..last].each do |player|
+    new_team.players.create(first_name: player[:first_name], last_name: player[:last_name])
+  end
+end
+
+teams = Team.all
+
+MATCH_COUNT.times do |i|
+  shuffled_teams = teams.shuffle
+  date = Time.now - i.days - i.hours
+
+  Match.create(
+    match_date: date,
+    home_team_id: shuffled_teams.first.id,
+    away_team_id: shuffled_teams.last.id
+  )
+end
+
+players = Player.all
+matches = Match.all
+
+ACHIEVEMENTS.each do |achievement|
+  new_achievement = Achievement.create(body: achievement)
+
+  players.each do |player|
+    next if rand(0..2) == 0
+    new_achievement.reach_by(player, matches.sample)
+  end
+end
